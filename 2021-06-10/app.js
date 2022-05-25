4d565a32c99ec26b923599e67a2ef2de3595beff
Marcos Hopkins <99453958+mhopkinss@users.noreply.github.com>
2022-05-24 19:18:01 -0700

    Update app.js

--- 2021-06-10/app.js
+++ 2021-06-10/app.js
@@ -2,8 +2,7 @@
 //r: return the number of litres nathan will drink, rounded to smallest value
 //e: litres(2), 1, 'should return 1 litre'
   //litres(12.3), 6, 'should return 6 litres'
-//p: take in an hour, round it to lowest number, divide that hour by 2 to get litres
-
+//p: take in an hour, round product of time * 0.5 to lowest integer
 function litres(time) {
     return Math.floor(time * 0.5);
   }
